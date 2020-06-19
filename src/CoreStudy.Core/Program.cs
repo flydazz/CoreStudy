2f9c989120204bb8df8348a9a71b5d6e35016dab
bunger <770181556@qq.com>
2020-06-19 11:24:56 +0800

    first commit,init project

---
+++ src/CoreStudy.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreStudy.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
